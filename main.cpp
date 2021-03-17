96c013f617fd47cc2a459909cca9dd7047c6f274
Sam Hurwitz <contact.samhurwitz@gmail.com>
2021-03-17 23:21:34 +0500

    Modify sandbox

--- main.cpp
+++ main.cpp
@@ -9,6 +9,10 @@ int main() {
   for (int i = 1; i < 8; i++) {
     for (int j = 3; j < 7; j++) {
       screen.at(i, j) = U'🍌';
+
+      if (i % 2 == 0 && j % 2 == 0) {
+        screen.charAt(i, j).Inverted = true;
+      }
     }
   }
 
