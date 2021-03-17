c94a2fbaff1b77f2d0716dd0b619be3e9ffca0ee
Sam Hurwitz <contact.samhurwitz@gmail.com>
2021-03-17 23:45:55 +0500

    Add ansi color benchmark

--- benchmarks/benchmark1.cpp
+++ benchmarks/benchmark1.cpp
@@ -97,4 +97,31 @@ static void BM_Draw_SGR(benchmark::State& state) {
 
 BENCHMARK(BM_Draw_SGR);
 
+
+static void BM_Draw_Color(benchmark::State& state) {
+  Screen screen;
+
+  for (int i = 1; i < 8; i++) {
+    for (int j = 3; j < 7; j++) {
+      screen.at(i, j) = U'*';
+
+      if (i % 2 == 0 && j % 2 == 0) {
+        screen.charAt(i, j).ForegroundColor = Color::Blue;
+        screen.charAt(i, j).BackgroundColor = Color::Yellow;
+      } else {
+        screen.charAt(i, j).ForegroundColor = Color::Cyan;
+        screen.charAt(i, j).BackgroundColor = Color::White;
+      }
+    }
+  }
+  
+  for (auto _ : state) {
+    screen.Draw();
+  }
+
+  Terminal::restore();
+}
+
+BENCHMARK(BM_Draw_Color);
+
 BENCHMARK_MAIN();
